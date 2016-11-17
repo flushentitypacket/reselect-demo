77e205a2c72c95993440ae55c94bbc5855ed6670
Ronald Hong <ron@wistia.com>
2016-11-17 12:45:47 -0500

    Add redux-logger.

--- todo/index.jsx
+++ todo/index.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import { render } from 'react-dom'
 import { Provider } from 'react-redux'
-import { createStore } from 'redux'
+import { createStore, applyMiddleware } from 'redux'
+import createLogger from 'redux-logger'
 import todoApp from './reducers'
 import App from './components/App'
 
-let store = createStore(todoApp)
+const store = createStore(todoApp, applyMiddleware(createLogger()))
 
 render(
   <Provider store={store}>
