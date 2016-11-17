7b6e7e0e94e4f26e0f26b81dff05747519cd0262
Ronald Hong <ron@wistia.com>
2016-11-17 12:40:54 -0500

    Configure todo app.

--- webpack.config.js
+++ webpack.config.js
@@ -1,17 +1,17 @@
 module.exports = {
   devtool: 'source-map',
-  entry: './index.js',
+  entry: './todo/index.jsx',
   output: {
     path: './',
     filename: 'bundle.js'
   },
   module: {
     loaders: [{
-      test: /\.js$/,
+      test: /\.jsx?/,
       exclude: /node_modules/,
       loader: 'babel',
       query: {
-        presets: ['es2015']
+        presets: ['es2015', 'react']
       }
     }]
   }
