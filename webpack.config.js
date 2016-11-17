17445e06b249a3b47addaeb042d8cfa51d1a7be4
Ronald Hong <ron@wistia.com>
2016-11-17 12:19:44 -0500

    Add basic webpack configuration.

---
+++ webpack.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  devtool: 'source-map',
+  entry: './index.js',
+  output: {
+    path: './',
+    filename: 'bundle.js'
+  },
+  module: {
+    loaders: [{
+      test: /\.js$/,
+      exclude: /node_modules/,
+      loader: 'babel',
+      query: {
+        presets: ['es2015']
+      }
+    }]
+  }
+}
